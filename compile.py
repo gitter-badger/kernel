2d6e1430c97fe1c314dd30f8adfccb01df19f1e9
Matheus de Alcantara <matheus.de.alcantara@gmail.com>
2015-02-14 13:12:17 -0200

    Initial commit!

---
+++ compile.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+import fnmatch
+
+assembler = "C:\\Program Files\\CrossGCC\\bin\\i686-elf-as.exe"
+cCompiler = "C:\\Program Files\\CrossGCC\\bin\\i686-elf-gcc.exe"
+
+objDirs = ['.\\obj/i686/boot']
+
+cFiles = []
+for root, dirnames, filenames in os.walk('src'):
+	for filename in fnmatch.filter(filenames, '*.c'):
+		cFiles.append(os.path.join(root, filename))
+
+asmFiles = []
+for root, dirnames, filenames in os.walk('src'):
+	for filename in fnmatch.filter(filenames, '*.S'):
+		asmFiles.append(os.path.join(root, filename))
+
+objectFiles = []
+for root, dirnames, filenames in os.walk('obj'):
+	for filename in fnmatch.filter(filenames, '*.o'):
+		objectFiles.append(os.path.join(root, filename))
+
+for x in objDirs:
+	if not os.path.exists(x):	
+		os.makedirs(x)
+
+
+
+for asmSourceFile in asmFiles:
+	subprocess.call([assembler, asmSourceFile, '-o', asmSourceFile.replace('src', '.\\obj').replace('.S', '.o')])
+
+for cSourceFile in cFiles:
+	subprocess.call([cCompiler, '-c', cSourceFile, '-o', cSourceFile.replace('src', '.\\obj').replace('.c', '.o'),
+	 '-std=gnu99', '-ffreestanding', '-O2', '-Wall', '-Wextra'])
+
+
+linkerParams = ['-T', '.\\linker.ld', '-o', '.\\summit.bin', '-ffreestanding', '-O2', '-nostdlib', '-lgcc']
+
+
+for objectFile in objectFiles:
+	linkerParams.append(objectFile)
+
+subprocess.call([cCompiler] + linkerParams)
