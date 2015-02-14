5e728af67b94058f85023c566fc4dad1d31467f2
Matheus de Alcantara <matheus.de.alcantara@gmail.com>
2015-02-14 15:00:33 -0200

    Add build script arguments
    compile.py help for more info

--- compile.py
+++ compile.py
@@ -2,45 +2,121 @@
 import subprocess
 import os
 import fnmatch
+import sys
+import shutil
 
-assembler = "i686-elf-as"
-cCompiler = "i686-elf-gcc"
+def clean():
+	print ("--- Summit Compilation Helper --- ")
+	print ("Operation: Clean")
+	if os.path.exists("./summit.bin"):
+		os.remove("./summit.bin")
 
-objDirs = ['./obj/i686/boot']
+	if os.path.exists("obj") and os.path.isdir("obj"):
+		shutil.rmtree("obj")
 
-cFiles = []
-for root, dirnames, filenames in os.walk('src'):
-	for filename in fnmatch.filter(filenames, '*.c'):
-		cFiles.append(os.path.join(root, filename))
-
-asmFiles = []
-for root, dirnames, filenames in os.walk('src'):
-	for filename in fnmatch.filter(filenames, '*.S'):
-		asmFiles.append(os.path.join(root, filename))
-
-objectFiles = []
-for root, dirnames, filenames in os.walk('obj'):
-	for filename in fnmatch.filter(filenames, '*.o'):
-		objectFiles.append(os.path.join(root, filename))
-
-for x in objDirs:
-	if not os.path.exists(x):	
-		os.makedirs(x)
+	print ("Clean: done.")
+def build():
+	print ("--- Summit Compilation Helper --- ")
+	print ("Operation: Build")
+	assembler = "i686-elf-as"
+	cCompiler = "i686-elf-gcc"
 
 
+	cFiles = []
+	print("Updating list of C files.")
+	for root, dirnames, filenames in os.walk("src"):
+		for filename in fnmatch.filter(filenames, "*.c"):
+			cFiles.append(os.path.join(root, filename))
+			print("Adding C file ", filename, " to list of C files.")
+	print("-----------------")
+	asmFiles = []
+	print("Updating list of assembly files.")
+	for root, dirnames, filenames in os.walk("src"):
+		for filename in fnmatch.filter(filenames, "*.S"):
+			asmFiles.append(os.path.join(root, filename))
+			print("Adding assembly file ", filename, " to list of assembly files.")
+	print("-----------------")
 
-for asmSourceFile in asmFiles:
-	subprocess.call([assembler, asmSourceFile, '-o', asmSourceFile.replace('src', './obj').replace('.S', '.o')])
 
-for cSourceFile in cFiles:
-	subprocess.call([cCompiler, '-c', cSourceFile, '-o', cSourceFile.replace('src', './obj').replace('.c', '.o'),
-	 '-std=gnu99', '-ffreestanding', '-O2', '-Wall', '-Wextra'])
+	# Determine if all obj dirs are around
+	print("Determining if nescessary object directories are present.")
+
+	for cSourceFile in cFiles:
+		if not os.path.exists(os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj")):
+			os.makedirs(os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj"))
+			print("Made directory ", os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj"))
+
+	for asmFile in cFiles:
+		if not os.path.exists(os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj")):
+			os.makedirs(os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj"))
+			print("Made directory ", os.path.dirname(os.path.abspath(cSourceFile)).replace("src", "obj"))
+	print("-----------------")
+	print("Directories up-to-date.\nCompiling.")
+	# Build all files
+
+	for asmSourceFile in asmFiles:
+		print("Assembling ", asmSourceFile, " to ", asmSourceFile.replace("src", "obj").replace(".S", ".o"))
+		subprocess.call([assembler, asmSourceFile, "-o", asmSourceFile.replace("src", "obj").replace(".S", ".o")])
+
+	for cSourceFile in cFiles:
+		print("Compiling ", cSourceFile, " to ", cSourceFile.replace("src", "obj").replace(".c", ".o"))
+		subprocess.call([cCompiler, "-c", cSourceFile, "-o", cSourceFile.replace("src", "obj").replace(".c", ".o"),
+		 "-std=gnu99", "-ffreestanding", "-O2", "-Wall", "-Wextra"])
+
+	print("Compilation finished.\n-----------------\nLinking kernel to summit.bin.\n")
+
+	linkerParams = ["-T", "./linker.ld", "-o", "./summit.bin", "-ffreestanding", "-O2", "-nostdlib", "-lgcc"]
+
+	print("Updating list of object files.")
+	objectFiles = []
+	for root, dirnames, filenames in os.walk("obj"):
+		for filename in fnmatch.filter(filenames, "*.o"):
+			print("Adding ", filename, " to object file list.")
+			objectFiles.append(os.path.join(root, filename))
 
 
-linkerParams = ['-T', './linker.ld', '-o', './summit.bin', '-ffreestanding', '-O2', '-nostdlib', '-lgcc']
 
+	for objectFile in objectFiles:
+		linkerParams.append(objectFile)
 
-for objectFile in objectFiles:
-	linkerParams.append(objectFile)
+	print("Invoking linker.\n-----------------")
+	subprocess.call([cCompiler] + linkerParams)
 
-subprocess.call([cCompiler] + linkerParams)
+	print ("Build: done.")
+
+def pull():
+	print ("--- Summit Compilation Helper --- ")
+	print ("Operation: Pull")
+	subprocess.call(["git", "pull", "origin", "master"])
+	print ("Pull: done.")
+
+def update():
+	print ("--- Summit Compilation Helper --- ")
+	print ("Operation: Update")
+
+	clean()
+	pull()
+
+	build()
+
+	print ("Update: done.")
+
+if __name__ == "__main__":
+	
+	if sys.argv.__len__() > 1:
+		for x in sys.argv[1:]:
+			if x == "clean":
+				clean()
+			elif x == "build":
+				build()
+			elif x == "pull":
+				pull()
+			elif x == "update":
+				update()
+			elif x == "help":
+				print("Valid options: \nclean : deletes object directories and object files \nbuild : builds the current source tree\npull: pulls the source tree from git\nupdate: cleans, updates and builds the source tree.")
+			elif x == "start-emu":
+				subprocess.call(["qemu-system-i386", "-kernel", "summit.bin"])
+	else:
+		print("Usage: compile.py argument")
+		print("Example usage:\n\tcompile.py help")
